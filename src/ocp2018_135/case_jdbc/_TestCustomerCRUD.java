634c8f98ffb599d2b836aeb50e2189f9c991746f
TiramiAsu <46002098+TiramiAsu@users.noreply.github.com>
2020-02-20 01:53:49 +0800

    +ADD [ case ] Customers 操作: [ C ] R U D

---
+++ src/ocp2018_135/case_jdbc/_TestCustomerCRUD.java
@@ -0,0 +1,61 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * &copy;TiramiAsu
+ * 
+ */
+package ocp2018_135.case_jdbc;
+
+import ocp2018_135.case_jdbc.beans.Customer;
+import ocp2018_135.case_jdbc.dao.CustomerDAO;
+import ocp2018_135.case_jdbc.dao.impl.CustomerDAOImpl;
+
+/**
+ * <pre>
+ * [測試客戶 CRUD] 2020-02-19 15:10
+ * </pre>
+ * 
+ * @author TiramiAsu (Email)
+ */
+public class _TestCustomerCRUD {
+
+	private static CustomerDAO customerDAO = new CustomerDAOImpl();
+
+	/*
+	CREATE TABLE public.customers
+	(
+		id serial NOT NULL,
+		code character varying(255) NOT NULL,
+		name character varying(255) NOT NULL,
+		phone character varying(127) NOT NULL,
+		address character varying(1023) NOT NULL,
+		relationman character varying(127) NOT NULL,
+		remark character varying(1023),
+		PRIMARY KEY (id)
+	)
+	WITH (OIDS = FALSE);
+
+	ALTER TABLE public.customers OWNER to postgres;
+	*/
+	public static void main(String[] args) {
+
+		System.out.println(">>> 新增客戶 <<<");
+		create();
+	}
+
+	public static void create() {
+
+		// 新增客戶資料
+		Customer customer1 = new Customer("K8561", "Jeff", "0974185293", "oo市oo區oo路二段xx號5樓", "Harry", null);
+		Customer customer2 = new Customer("F8852", "Harry", "0944278557", "oo市oo區oo路三段xx號7樓", "Lea", "nice person");
+		Customer customer3 = new Customer("S1259", "Lea", "0922727448", "oo市oo區oo路一段xx號3樓", "Vivi", null);
+
+		// 新增客戶
+		customerDAO.create(customer1); // 資料已經進資料庫，所以 static 覆蓋沒有關係(購物車就不可用這方式)
+		customerDAO.create(customer2);
+		customerDAO.create(customer3);
+	}
+}
--- src/ocp2018_135/case_jdbc/dao/impl/CustomerDAOImpl.java
+++ src/ocp2018_135/case_jdbc/dao/impl/CustomerDAOImpl.java
@@ -12,10 +12,12 @@ package ocp2018_135.case_jdbc.dao.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import ocp2018_135.case_jdbc.beans.Customer;
 import ocp2018_135.case_jdbc.dao.CustomerDAO;
+import ocp2018_135.case_jdbc.db.DatabaseManager;
 
 /**
  * <pre>
@@ -32,8 +34,41 @@ public class CustomerDAOImpl implements CustomerDAO {
 
 	@Override
 	public void create(Customer customer) {
-		// TODO Auto-generated method stub
 
+		// 連接資料庫
+		try {
+			// 建構 Driver 並取得連結
+			conn = DatabaseManager.getInstance().getConnection();
+
+			// 建立資料
+			String sql = "INSERT INTO customers(id, code, name, phone, address, relationman, remark) "
+					+ "VALUES(default, ?, ?, ?, ?, ?, ?);";
+
+			// prepare execute SQL
+			ps = conn.prepareStatement(sql);
+			System.out.println(">>> create SQL: " + ps);
+
+			// set data to SQL
+			ps.setString(1, customer.getCode());
+			ps.setString(2, customer.getName());
+			ps.setString(3, customer.getPhone());
+			ps.setString(4, customer.getAddress());
+			ps.setString(5, customer.getRelationman());
+			ps.setString(6, customer.getRemark());
+
+			// 執行 SQL
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 無論如何都會執行, 所以在這裡關掉 ps, conn
+			try {
+				DatabaseManager.getInstance().close(ps);
+				DatabaseManager.getInstance().close(conn);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
